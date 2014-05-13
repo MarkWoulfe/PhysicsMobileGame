35341b90bc487d485de033f9ed428608c46fd51c
Maximo1491 <maximo1491@hotmail.com>
2014-05-13 17:24:27 +0100

    Added cannon script

---
+++ Assets/Scripts/Cannon_Pitch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon_Pitch : MonoBehaviour {
+
+	public int rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("up"))
+			transform.Rotate (0, 0, rotateSpeed);
+		if(Input.GetKey("down"))
+			transform.Rotate (0, 0, -rotateSpeed);
+	}
+}
