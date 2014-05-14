341fc87e6183f12006b7b806fdf97c5a4e4b8821
Mark Woulfe <mark@markwoulfe.co.uk>
2014-05-14 16:49:30 +0100

    Added cannonball physics

---
+++ Assets/Scripts/cannon_ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cannon_ball : MonoBehaviour {
+
+	public float power;
+	public Vector3 angle;
+
+	void start(){
+		angle.Normalize();
+	}
+
+	void Update() {
+
+		if (Input.GetKeyDown ("space")) {
+			rigidbody.AddForce(angle * power);			
+		}
+
+	}
+
+}
