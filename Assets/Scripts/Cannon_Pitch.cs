1af6800d0f5089354e957c06d8e67e0ae530587e
alexcbluk <alexcbluk@hotmail.com>
2014-05-14 14:37:24 +0100

    Yolo cannon limit swag

--- Assets/Scripts/Cannon_Pitch.cs
+++ Assets/Scripts/Cannon_Pitch.cs
@@ -12,9 +12,23 @@ public class Cannon_Pitch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("up"))
-			transform.Rotate (0, 0, rotateSpeed);
-		if(Input.GetKey("down"))
-			transform.Rotate (0, 0, -rotateSpeed);
+        Vector3 angle = transform.localEulerAngles;
+
+        if (Input.GetKey("up") && (angle.z <= 45.1f || angle.z >= 329.9f))
+            transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+        if (Input.GetKey("down") && (angle.z <= 45.1f || angle.z >= 329.9f))
+            transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+
+        angle = transform.localEulerAngles;
+
+        if (angle.z > 45 && angle.z < 60)
+            angle.z = 45;
+
+        if (angle.z > 310 && angle.z < 330)
+            angle.z = 330;
+
+        transform.localEulerAngles = angle;
+
+        print(angle.z);
 	}
 }
