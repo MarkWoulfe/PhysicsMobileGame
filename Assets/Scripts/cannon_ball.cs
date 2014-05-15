22d4668605f963fa9cd2062d2231e51ee1229529
Maximo1491 <maximo1491@hotmail.com>
2014-05-15 23:22:28 +0100

    Added tap commands

--- Assets/Scripts/cannon_ball.cs
+++ Assets/Scripts/cannon_ball.cs
@@ -14,7 +14,7 @@ public class cannon_ball : MonoBehaviour {
 
 	void Update() {
 
-		if (Input.GetKeyDown ("space")) {
+		if (Input.GetKeyDown ("space") || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)) {
 
             angle = transform.position - cannon.transform.position;
 
