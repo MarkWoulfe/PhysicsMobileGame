e40524967968b8ec6fe2f1290c7332bcb841327f
Maximo1491 <maximo1491@hotmail.com>
2014-05-18 16:01:51 +0100

    Improved cannon

--- Assets/Scripts/Cannon_Pitch.cs
+++ Assets/Scripts/Cannon_Pitch.cs
@@ -9,8 +9,6 @@ public class Cannon_Pitch : MonoBehaviour
     public int rotateSpeed = 10;
     public GameObject cannonBall;
 
-    float scaleValue = 0.01f;
-
 	//have the slider scale to the users device
 	float sliderWidth = Screen.width * 0.4f;
 	float sliderHeight = Screen.height-50;
@@ -22,69 +20,24 @@ public class Cannon_Pitch : MonoBehaviour
 		cannonAngle = GUI.VerticalSlider(new Rect(25, 25, sliderWidth, sliderHeight), cannonAngle, 45.0f, 0.0f);
 	}
 
-    void start()
-    { 
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         Vector3 angle = transform.localEulerAngles;
-        bool pitchedTurret = false;
 
 		angle.z = cannonAngle;
 
-        if (Input.GetKey("up") && (angle.z < 45.1f || angle.z > 359.9f))
-        {
-            transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
-            pitchedTurret = true;
-        }
-        if (Input.GetKey("down") && (angle.z < 45.1f || angle.z > 359.9f))
-        {
-            transform.Rotate(Vector3.forward, -rotateSpeed * Time.deltaTime);
-            pitchedTurret = true;
-        }
         if (Input.GetKeyDown("space") || (Input.touchCount == 2 && Input.GetTouch(1).phase == TouchPhase.Began))
         {
             fired = true;
         }
 
-        angle = transform.localEulerAngles;
-
-        if (angle.z > 45 && angle.z < 60)
-        {
-            angle.z = 45;
-            pitchedTurret = false;
-        }
-
-        if (angle.z > 310 && angle.z < 360)
-        {
-            angle.z = 0;
-            pitchedTurret = false;
-        }
-
         transform.localEulerAngles = angle;
 
-        if (pitchedTurret && Input.GetKey("up") && fired == false)
+        if(fired == true)
         {
-            cannonBall.transform.RotateAround(transform.position, Vector3.forward, rotateSpeed * Time.deltaTime);
+            fired = false;
+            cannonBall.GetComponent<cannon_ball>().fire(transform.position, angle.z);
         }
-        if (pitchedTurret && Input.GetKey("down") && fired == false)
-        {
-            cannonBall.transform.RotateAround(transform.position, Vector3.forward, -rotateSpeed * Time.deltaTime);
-        }
-
-        if (scaleValue < 20 && fired == true)
-            scaleValue += 2.0f;
-
-        if(scaleValue > 20)
-        {
-            cannonBall.GetComponent<cannon_ball>().fire();
-
-            scaleValue = 20;
-        }
-
-        cannonBall.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
     }
 }
--- Assets/Scripts/cannon_ball.cs
+++ Assets/Scripts/cannon_ball.cs
@@ -16,8 +16,12 @@ public class cannon_ball : MonoBehaviour {
 
 	}
 
-    public void fire()
+    public void fire(Vector3 p, float a)
     {
+        transform.RotateAround(p, Vector3.forward, a);
+
+        transform.localScale = new Vector3(20, 20, 20);
+
         angle = transform.position - cannon.transform.position;
 
         angle.Normalize();
