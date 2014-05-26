e3a843d8f7100c44bb29e0ba95f3b5cae9f043af
Mark Woulfe <mark@markwoulfe.co.uk>
2014-05-26 17:17:14 +0100

    added buttons to menu

---
+++ Assets/Scripts/ballSelect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballSelect : MonoBehaviour {
+
+	public Texture football;
+	public Texture bowlingball;
+	public Texture leadball;
+	public Texture cannonball;
+
+	float buttonWidth, buttonHeight;
+
+	void Start () {
+
+		buttonWidth = (Screen.width / 2) - 50;
+		buttonHeight = (Screen.height / 2) - 50;
+
+	}
+
+	void OnGUI(){
+
+		if (GUI.Button(new Rect(50, 50, buttonWidth, buttonHeight), "Football"))
+			Debug.Log("1");
+
+		if (GUI.Button(new Rect(50 + buttonWidth, 50, buttonWidth, buttonHeight), "Bowlingball"))
+			Debug.Log("2");
+
+		if (GUI.Button(new Rect(50, 50+buttonHeight, buttonWidth, buttonHeight), "Leadball"))
+			Debug.Log("3");
+
+		if (GUI.Button(new Rect(50 + buttonWidth, 50+buttonHeight, buttonWidth, buttonHeight), "Cannonball"))
+			Debug.Log("4");
+
+	}
+}
