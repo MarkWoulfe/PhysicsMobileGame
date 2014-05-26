f873c17afca41c755a7e0c21d32436d00253438e
Mark Woulfe <mark@markwoulfe.co.uk>
2014-05-26 14:17:17 +0100

    added android closing script and new menu scene

---
+++ Assets/Scripts/androidClose.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class androidClose : MonoBehaviour {
+	
+	// Quit the app when hitting the back button
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit(); 
+	}
+
+}
