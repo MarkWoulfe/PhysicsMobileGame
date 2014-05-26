df950c4eca508b31c0b813e5a5e4045ced550785
Maximo1491 <maximo1491@hotmail.com>
2014-05-26 20:01:07 +0100

    Made cannon balls better :D

--- Assets/Scripts/cannon_ball.cs
+++ Assets/Scripts/cannon_ball.cs
@@ -12,15 +12,23 @@ public class cannon_ball : MonoBehaviour {
         //Rotate the ball so that it is at the end of the cannon
         transform.RotateAround(p, Vector3.forward, a);
 
-        //Scale the object up so that the object is not at 0, 0, 0 (default value)
-        transform.localScale = new Vector3(1, 1, 1);
+        if (rigidbody.mass > 15)
+        {
+            //Smaller scale for the lead ball for greater effect of mass.
+            transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        }
+        else
+        {
+            //Scale the object up so that the object is not at 0, 0, 0 (default value)
+            transform.localScale = new Vector3(1, 1, 1);
+        }
 
         //Gets the direction between the cannon and the ball
         Vector3 dir = transform.position - p;
         //Normalises the vector
         dir.Normalize();
         //Adds force in the correct direction
-        rigidbody.AddForce(dir * pow);
+        rigidbody.AddForce(dir * (pow * rigidbody.mass));
         rigidbody.AddTorque(new Vector3(0,0,-1) * pow);
         //Makes sure that gravity is acting on the ball
         rigidbody.useGravity = true;
