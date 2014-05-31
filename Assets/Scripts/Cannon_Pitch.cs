6df18fffb70d14e84167d339920db08eac0bb1fc
MarkWoulfe <mark@markwoulfe.co.uk>
2014-05-31 13:15:18 +0100

    Labels added for both angle and power sliders

--- Assets/Scripts/Cannon_Pitch.cs
+++ Assets/Scripts/Cannon_Pitch.cs
@@ -13,6 +13,7 @@ public class Cannon_Pitch : MonoBehaviour
 		float shotTimer = 1;
 		float shotCount = 0;	
 		float shotLimit = 3;
+		//default low power
 		float pow = 1000;
 		bool firePressed = false;
 		bool disableGUI = false;
@@ -22,34 +23,42 @@ public class Cannon_Pitch : MonoBehaviour
 		//have the slider scale to the users device
 		float sliderWidth = Screen.width * 0.05f;
 		float sliderHeight = Screen.height - 50;
-
+	
 		//Keeps track if the cannon has been fired or not
 		bool fired = false;
 
 		//draw our GUI elements
 		void OnGUI ()
 		{
-
+				//while our ui is not disabled
 				if (!disableGUI) {
 
-		 
 						//use a custom skin
 						GUI.skin = myGUISkin;
-						//make sure our slider scales to the device
+						//make sure our sliders scales to the device
 						GUI.skin.verticalSlider.fixedWidth = sliderWidth;
 						GUI.skin.verticalSliderThumb.fixedWidth = sliderWidth;
-						
 						GUI.skin.horizontalSlider.fixedHeight = sliderWidth;
 						GUI.skin.horizontalSliderThumb.fixedHeight = sliderWidth;
-						
+						//font size based on device (so you can see it well)
 						GUI.skin.button.fontSize = (int)sliderWidth;
-						//draw our slider
+						GUI.skin.label.fontSize = (int)sliderWidth-5;
+						GUI.skin.label.alignment = TextAnchor.UpperCenter;
+						//some label rotation for our angle indicator (4x4 backup acting as a push/pop)
+						Matrix4x4 backup = GUI.matrix;
+						GUIUtility.RotateAroundPivot(-90, new Vector2(25+sliderWidth/2, sliderHeight));
+						GUI.Label (new Rect ((sliderWidth - (sliderWidth/2)), sliderHeight-(sliderWidth/2) - 10, sliderHeight, sliderWidth*2), "Angle");
+						GUI.matrix = backup;
+						//draw our angle slider
 						cannonAngle = GUI.VerticalSlider (new Rect (25, 25, sliderWidth, sliderHeight), cannonAngle, 45.0f, 0.0f);
+						//draw our power slider with label
+						GUI.Label (new Rect (25 + sliderWidth + 25, 20, (Screen.width/2) - (sliderWidth + 50), sliderWidth * 2), "Power");
 						pow = GUI.HorizontalSlider (new Rect (25 + sliderWidth + 25, 25, (Screen.width/2) - (sliderWidth + 50), sliderWidth * 2), pow, 1000.0f, 3000.0f);
-			
+						//button for firing the cannonball
 						if (GUI.Button (new Rect (25 + sliderWidth + 25, 100 + sliderWidth, (Screen.width/2) - (sliderWidth + 50), sliderWidth * 2), "Fire")) {
 								firePressed = true;
 						}
+
 				}
 
 		}
@@ -97,23 +106,6 @@ public class Cannon_Pitch : MonoBehaviour
 						firePressed = false;
 				}
 
-				//legacy controls
-				/*//If the space key is pressed or a mobile screen is tapped and the cannon has not already been fired
-				if ((Input.GetKeyDown ("space") || (Input.touchCount == 2 && Input.GetTouch (1).phase == TouchPhase.Began)) && fired == false && newCannonBall.rigidbody.velocity.x < 1) {
-						fired = true;
-
-						//Creates a new cannon ball based on the defaults defined in the editor
-						newCannonBall = (GameObject)Instantiate (cannonBall1);
-						//Moves the new cannonball to the position of the original
-						newCannonBall.transform.position = cannonBall1.transform.position;
-						//Call the fire function for the new cannon ball
-						newCannonBall.GetComponent<cannon_ball> ().fire (transform.position, angle.z, pow);
-						//Reset the timer back to 0
-						shotTimer = 0;
-						//Increase the amount of shots taken
-						shotCount++;
-				}*/
-
 				//Keeps track of how long the balls velocity is less than 1
 				if (newCannonBall.rigidbody.velocity.x < 1 && newCannonBall.rigidbody.velocity.y < 1) {
 						shotTimer += Time.deltaTime;
@@ -122,9 +114,10 @@ public class Cannon_Pitch : MonoBehaviour
 				}
 
 				//If the cannon balls velocity is less than 1 for more than 2 seconds jump back to the cannon
-				if (shotTimer < 1.5f) {
+				if (shotTimer < 1) {
 						//Moves the camera to follow the cannon
 						gameCameraScript.MoveCamera (newCannonBall.transform.position);
+						//stop drawing the gui
 						disableGUI = true;
 				} else {
 						//Allow another shot to be fired it is less than the limit
@@ -136,6 +129,7 @@ public class Cannon_Pitch : MonoBehaviour
 						}
 						//Moves the camera to follow the new cannon ball
 						gameCameraScript.MoveCamera (cannonBall.transform.position);
+						//redraw the gui
 						disableGUI = false;
 				}
 		}
